4219dfd850a4b8377c277ce3e8e129422ccab366
notzie <notykt@gmail.com>
2021-09-08 12:45:54 +0900

    Fix constraints

--- SteamHoursChecker/Services/SteamWebAPI.swift
+++ SteamHoursChecker/Services/SteamWebAPI.swift
@@ -8,30 +8,27 @@
 import Foundation
 
 class SteamWebAPI {
-	private var url: URL?
+    public func request(apiKey: String, steamId: String, completionBlock: @escaping (GetRecentlyPlayedGames) -> Void) {
+        let link: String = "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/"
 
-	private func createURL(apiKey: String, steamId: String) {
-        url = URL(string: "http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=\(apiKey)&steamid=\(steamId)&format=json")!
-	}
+        guard let url = URL(string: link + "?key=\(apiKey)&steamid=\(steamId)&format=json") else {
+            print("wrong url")
+            return
+        }
 
-	public func request(apiKey: String, steamId: String, completionBlock: @escaping (GetRecentlyPlayedGames) -> Void) {
-		   createURL(apiKey: apiKey, steamId: steamId)
-		   guard let url = url else {
-			   print("wrong url")
-			   return
-		   }
-		   var request = URLRequest(url: url)
-		   request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-		   URLSession.shared.dataTask(with: url) { data, _, error in
-			   if let data = data {
-				   if let ans = try? JSONDecoder().decode(GetRecentlyPlayedGames.self, from: data) {
-					   completionBlock(ans)
-				   } else {
-					   print("invalid")
-				   }
-			   } else if let error = error {
-				   print("URLSession data task error: \(error)")
-			   }
-		   }.resume()
-	}
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            if let data = data {
+                if let ans = try? JSONDecoder().decode(GetRecentlyPlayedGames.self, from: data) {
+                    completionBlock(ans)
+                } else {
+                    print("invalid")
+                }
+            } else if let error = error {
+                print("URLSession data task error: \(error)")
+            }
+        }.resume()
+    }
 }
